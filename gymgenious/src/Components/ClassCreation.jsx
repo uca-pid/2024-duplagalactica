a369d58fea8d875753a469c6ff0227e07b44f62f
JuanLopez25 <114444407+JuanLopez25@users.noreply.github.com>
2024-09-06 22:12:56 -0300

    making left bar changes to be able to merge

--- gymgenious/src/Components/ClassCreation.jsx
+++ gymgenious/src/Components/ClassCreation.jsx
@@ -41,7 +41,22 @@ export default function CreateClass() {
 
   return (
     <div className='App'>
-      <LeftBar/>
+      <div className='Left-Bar'>
+        <div className='Logo-Container'>
+          <svg className='Container-Logo' viewBox="0 0 220 210">
+            <defs>
+              <path id="circlePath" d="M 110,100 m -90,0 a 90,90 0 1,1 180,0 a 90,90 0 1,1 -180,0" />
+            </defs>
+            <circle cx="100" cy="100" r="90" fill="#14213D" />
+            <image href="/LogoGymGenius.png" x="10" y="10" height="180" width="180" />
+            <text>
+              <textPath href="#circlePath" className="Circle-Text">
+                GymGenius GymGenius GymGenius GymGenius GymGenius GymGenius GymGenius GymGenius
+              </textPath>
+            </text>
+          </svg>
+        </div>
+      </div>
       <div className='login-container'>
         <h2>Crear clase</h2>
         <form onSubmit={handleSubmit}>
--- gymgenious/src/Components/Main_Page.jsx
+++ gymgenious/src/Components/Main_Page.jsx
@@ -60,7 +60,22 @@ export default function Main_Page() {
 
   return (
     <div className="App">
-      <LeftBar/>
+      <div className='Left-Bar'>
+        <div className='Logo-Container'>
+          <svg className='Container-Logo' viewBox="0 0 220 210">
+            <defs>
+              <path id="circlePath" d="M 110,100 m -90,0 a 90,90 0 1,1 180,0 a 90,90 0 1,1 -180,0" />
+            </defs>
+            <circle cx="100" cy="100" r="90" fill="#14213D" />
+            <image href="/LogoGymGenius.png" x="10" y="10" height="180" width="180" />
+            <text>
+              <textPath href="#circlePath" className="Circle-Text">
+                GymGenius GymGenius GymGenius GymGenius GymGenius GymGenius GymGenius GymGenius
+              </textPath>
+            </text>
+          </svg>
+        </div>
+      </div>
       <div className="Calendar-Button">
         <button onClick={changeShowCalendar} className="Toggle-Button">
           {showCalendar ? (
