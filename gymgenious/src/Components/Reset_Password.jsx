a6af690cf728f21f1303d2ca50a0b1b918c4eb2b
JuanLopez25 <114444407+JuanLopez25@users.noreply.github.com>
2024-09-07 09:11:06 -0300

    adding the logic to alert the user when the email does not exist and try to recover it

--- gymgenious/src/Components/Reset_Password.jsx
+++ gymgenious/src/Components/Reset_Password.jsx
@@ -3,22 +3,24 @@ import React, { useState } from 'react';
 import LeftBar from '../real_components/LeftBar.jsx';
 import { useNavigate } from 'react-router-dom';
 import { getAuth,sendPasswordResetEmail } from 'firebase/auth';
+import { getUniqueUserByEmail } from '../firestoreService'; 
 
 export default function ResetPassword() {
     const [email, setEmail] = useState('');
+    const [showNotMatchEmail, setShowNoMatchEmail] = useState(false)
     const navigate = useNavigate();
 
     const auth = getAuth();
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         try {
+            const data = await getUniqueUserByEmail(email); 
             await sendPasswordResetEmail(auth, email); 
             alert('¡Correo electrónico enviado con éxito!');
-            navigate('/'); // Redirige a la página principal o a una página de confirmación
+            navigate('/'); 
         } catch (error) {
+            setShowNoMatchEmail(true)
             console.error("Error al enviar el correo electrónico:", error);
-            alert('Error al enviar el correo electrónico. Verifica tu conexión y el correo electrónico ingresado.');
         }
     };
 
@@ -44,6 +46,11 @@ export default function ResetPassword() {
                     <button type="submit" className='button_create_account'>
                         Enviar mail de recuperación
                     </button>
+                    {showNotMatchEmail ? (
+                        <div>No existe el correo electronico</div>
+                    ):(
+                        <div></div>
+                    )}
                 </form>
             </div>
         </div>
