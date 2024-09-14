66f5989f90e942813bf6daa3ecad1bbe75fd84df
Isoldios <isoldi772@gmail.com>
2024-09-14 16:10:10 -0300

    Adding a routine creation page

--- gymgenious/src/App.js
+++ gymgenious/src/App.js
@@ -10,6 +10,7 @@ import New_Password from './Components/New_Password.jsx';
 import Verify_email from './Components/VerifyEmail.jsx';
 import Redirections from './Components/redirections.jsx';
 import ExerciseCreation from './Components/ExerciseCreation.jsx';
+import RoutineCreation from './Components/RoutineCreation.jsx';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/verify-email" element={<Verify_email/>}/>
         <Route path="/redirections" element={<Redirections/>}/>
         <Route path="/exersice-creation" element={<ExerciseCreation/>}/>
+        <Route path="/routine-creation" element={<RoutineCreation/>}/>
       </Routes>
     </Router>
   );
