b56d40b061ac3797c67c723b150b9ae5763b0c85
Isoldios <isoldi772@gmail.com>
2024-09-20 01:53:49 -0300

    Changing alerts at class creation

--- gymgenious/src/Components/ClassCreation.jsx
+++ gymgenious/src/Components/ClassCreation.jsx
@@ -3,7 +3,13 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LeftBar from '../real_components/NewLeftBar.jsx';
 import moment from 'moment'
-import NewLeftBar from '../real_components/NewLeftBar.jsx'
+import Box from '@mui/material/Box';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Alert from '@mui/material/Alert';
+import CheckIcon from '@mui/icons-material/Check';
+import Slide from '@mui/material/Slide';
+import Popper from '@mui/material/Popper';
 
 export default function CreateClass() {
   const [hour, setHour] = useState('');
@@ -15,60 +21,99 @@ export default function CreateClass() {
   const urlParams = new URLSearchParams(window.location.search);
   const userMail = urlParams.get('mail');  
   const userType = urlParams.get('type');
+  const [errors, setErrors] = useState([]);
+  const [openCircularProgress, setOpenCircularProgress] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
+  const [failureErrors, setFailureErrors] = useState(false);
+
   const day = (dateString) => {
     const date = new Date(dateString);
     const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
     return daysOfWeek[date.getDay()];
   };
 
+  const validateForm = () => {
+      let errors = [];
+      const format= "HH:mm";
+      const realHourEnd = moment(hourFin, format).subtract(30, 'minutes').format(format);
+      if(moment(realHourEnd, format).isBefore(moment(hour, format))){
+        errors.push('Class must last at least 30 minutes.');
+      }
+
+      if(hour==='' || hourFin===''){
+        errors.push('Please enter both the start and end times.')
+      }
+
+      if (name === '') {
+          errors.push('Please enter an exercise name.');
+      }
+
+      if(permanent===''){
+        errors.push('Please enter if the class is recurring or not.');
+      }
+
+      if(date===''){
+        errors.push('Please enter a date.')
+      }
+
+      const today = new Date();
+      const inputDate = Date(date);
+      if (inputDate < today || (inputDate == today && moment(hour, format).isBefore(moment()))){
+        errors.push('Please enter a day and time after the current one');
+      }
+
+      setErrors(errors);
+      return errors.length === 0;
+  }
+
   const handleCreateClass = async () => {
-    const format= "HH:mm";
-    const realHourEnd = moment(hourFin, format).subtract(30, 'minutes').format(format);
-    try {
-      if(moment(realHourEnd, format).isBefore(moment(hour, format))){
-        throw new Error('La clase debe durar al menos 30 minutos');
+    setOpenCircularProgress(true);
+    if(validateForm()){
+      try {
+        const isoDateStringInicio = `${date}T${hour}:00Z`;
+        const isoDateStringFin = `${date}T${hourFin}:00Z`;
+        const newClass = {
+          name: name,
+          dateInicio: isoDateStringInicio,
+          dateFin: isoDateStringFin,
+          hour: hour,
+          day: day(date),
+          permanent: permanent,
+          owner: userMail
+        };
+        const response = await fetch('http://127.0.0.1:5000/create_class', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newClass),
+        });
+    
+        if (!response.ok) {
+          throw new Error('Error al crear la clase');
+        }
+        setOpenCircularProgress(false);
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+          navigate(`/?mail=${userMail}`, { state: { message: 'block' } });
+        }, 3000);
+      } catch (error) {
+        console.error("Error al crear la clase:", error);
+        setOpenCircularProgress(false);
+        setFailure(true);
+        setTimeout(() => {
+          setFailure(false);
+        }, 3000);
+        
       }
-  
-      const isoDateStringInicio = `${date}T${hour}:00Z`;
-      const isoDateStringFin = `${date}T${hourFin}:00Z`;
-  
-      const newClass = {
-        name: name,
-        dateInicio: isoDateStringInicio,
-        dateFin: isoDateStringFin,
-        hour: hour,
-        day: day(date),
-        permanent: permanent,
-        owner: userMail
-      };
-  
-      const response = await fetch('http://127.0.0.1:5000/create_class', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newClass),
-      });
-  
-      if (!response.ok) {
-        throw new Error('Error al crear la clase');
-      }
-  
-      navigate(`/?mail=${userMail}`, { state: { message: 'block' } });
-      alert("¡Clase creada exitosamente!");
-    } catch (error) {
-      console.error("Error al crear la clase:", error);
-      if(moment(realHourEnd, format).isBefore(moment(hour, format))){
-        alert('La clase debe durar al menos 30 minutos');
-      } else if (name=='') {
-      alert("Ingrese un nombre para la clase");
-     } else if (permanent=='') {
-      alert("Ingrese si la clase es recurrente o no");
-     } else if (date=='') {
-      alert("Ingrese la fecha de la clase");
-     } else {
-      alert("Error al crear la clase");
-     }
+    } else {
+      setOpenCircularProgress(false);
+      setFailureErrors(true);
+      setTimeout(() => {
+        setFailureErrors(false);
+      }, 3000);
     }
   };
   
@@ -78,9 +123,78 @@ export default function CreateClass() {
     handleCreateClass();
   };
 
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [openHourRequirements, setOpenHourRequirements] = useState(false);
+  const handleOpenHourRequirements = (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpenHourRequirements(!openHourRequirements)
+  };
+  const id = 'simple-popper';
+
+  const handleCloseHourRequirements = () => {
+    setOpenHourRequirements(false);
+  }
+
   return (
     <div className='full-screen-image-2'>
       <LeftBar email={userMail} type={userType}/>
+      {openCircularProgress ? (
+          <Backdrop
+          sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+          open={openCircularProgress}
+          >
+          <CircularProgress color="inherit" />
+          </Backdrop>
+      ) : null}
+      { success ? (
+          <div className='alert-container'>
+              <div className='alert-content'>
+                  <Box sx={{ position: 'relative', zIndex: 1 }}>
+                  <Alert style={{fontSize:'100%', fontWeight:'bold'}} icon={<CheckIcon fontSize="inherit" /> } severity="success">
+                      Class created successfully!
+                  </Alert>
+                  </Box>
+              </div>
+          </div>
+      ) : (
+          null
+      )}
+      { failureErrors ? (
+          <div className='alert-container'>
+              <div className='alert-content'>
+              <Box sx={{ position: 'relative', zIndex: 1 }}>
+                  <Slide direction="up" in={failureErrors} mountOnEnter unmountOnExit>
+                  <div>
+                      <Alert severity="error" style={{ fontSize: '100%', fontWeight: 'bold' }}>
+                      Error creating class
+                      </Alert>
+                      {errors.length > 0 && errors.map((error, index) => (
+                      <Alert key={index} severity="info" style={{ fontSize: '100%', fontWeight: 'bold' }}>
+                          <li>{error}</li>
+                      </Alert>
+                      ))}
+                  </div>
+                  </Slide>
+              </Box>
+              </div>
+          </div>
+        
+      ) : (
+          null
+      )}
+      { failure ? (
+          <div className='alert-container'>
+              <div className='alert-content'>
+              <Box sx={{ position: 'relative', zIndex: 1 }}>
+                  <Slide direction="up" in={failure} mountOnEnter unmountOnExit >
+                  <Alert severity="error" style={{fontSize:'100%', fontWeight:'bold'}}>Error creating class. Try again!</Alert>
+                  </Slide>
+              </Box>
+          </div>
+      </div>
+      ) : (
+          null
+      )}
       <div className='class-creation-container'>
         <div className='class-creation-content'>
           <h2 style={{color:'#5e2404'}}>Create class</h2>
@@ -88,7 +202,8 @@ export default function CreateClass() {
             <div className="input-container" style={{display:'flex', justifyContent: 'space-between'}}>
               <div className="input-small-container">
                 <label htmlFor="hour" style={{color:'#5e2404'}}>Start time:</label>
-                <input 
+                <input
+                  onClick={handleCloseHourRequirements}
                   type="time" 
                   id="hour" 
                   name="hour" 
@@ -98,17 +213,24 @@ export default function CreateClass() {
               </div>
               <div className="input-small-container">
                 <label htmlFor="hour" style={{color:'#5e2404'}}>End time:</label>
-                <input 
+                <input
+                  onClick={handleOpenHourRequirements}
                   type="time" 
                   id="hourFin" 
                   name="hourFin" 
                   value={hourFin} 
                   onChange={(e) => setHourFin(e.target.value)} 
                 />
+                <Popper id={id} open={openHourRequirements} anchorEl={anchorEl}>
+                  <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }} onClick={handleOpenHourRequirements}>
+                      <p>Class must last at least 30 minutes</p>
+                  </Box>
+                </Popper>
               </div>
               <div className="input-small-container">
                 <label htmlFor="name" style={{color:'#5e2404'}}>Name:</label>
-                <input 
+                <input
+                  onClick={handleCloseHourRequirements}
                   type="text" 
                   id="name" 
                   name="name" 
@@ -120,7 +242,8 @@ export default function CreateClass() {
             <div className="input-container" style={{display:'flex', justifyContent: 'space-between'}}>
               <div className="input-small-container" style={{width:"100%"}}>
                 <label htmlFor="permanent" style={{color:'#5e2404'}}>Recurrent:</label>
-                <select 
+                <select
+                  onClick={handleCloseHourRequirements}
                   id="permanent" 
                   name="permanent" 
                   value={permanent} 
@@ -133,7 +256,8 @@ export default function CreateClass() {
               </div>
               <div className="input-small-container" style={{ flex: 3, textAlign: 'left' }}>
                 <label htmlFor="date" style={{color:'#5e2404'}}>Date:</label>
-                <input 
+                <input
+                  onClick={handleCloseHourRequirements}
                   type="date" 
                   id="date" 
                   name="date" 
